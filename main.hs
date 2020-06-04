719c923ab4333e09bbe067aecaaa6fed1f4c4f71
SpIIIII <Shipin16@gmail.com>
2020-06-04 16:43:58 +0300

    v0.0.1 added symbol parser

--- main.hs
+++ main.hs
@@ -1,7 +1,20 @@
 module Main where
+
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 
+
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+
+readExpr :: String -> String
+readExpr input    = case parse symbol "lisp" input of
+                    Left err    -> "No match: " ++ show err
+                    Right val   -> "Found value"
 main :: IO()
 main = do
-        args <- getArgs
-        putStrLn ("Hello, " ++ args !! 0)
+        (expr:_) <- getArgs
+        putStrLn (readExpr expr)
